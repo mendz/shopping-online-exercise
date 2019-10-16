6aa51e2f2c2fc01180efc29a36bea52b3a0ce1c3
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-10-17 01:04:13 +0300

    fetch user then fetch cart

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
+import { map, tap, mergeMap, flatMap, switchMap } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CartProduct } from 'src/app/cart/cart-product.model';
+import { CartService } from 'src/app/cart/cart.service';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +20,12 @@ export class LoginComponent implements OnInit {
   error: string = null;
   isLoading = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private apiService: ApiService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     if (this.authService.user.value) {
@@ -41,17 +51,29 @@ export class LoginComponent implements OnInit {
 
   onLogin(email: string, password: string) {
     this.isLoading = true;
-    this.authService.login(email, password).subscribe(
-      responseData => {
-        this.isLoading = false;
-        this.router.navigate(['/products']);
-      },
-      errorMessage => {
-        this.isLoading = false;
-        this.error = errorMessage;
-        this.showPermissionIssue = true;
-      }
-    );
+    this.authService
+      .login(email, password)
+      .pipe(
+        map(
+          responseData => {
+            this.isLoading = false;
+            this.router.navigate(['/products']);
+            return responseData.localId;
+          },
+          (errorMessage: string) => {
+            this.isLoading = false;
+            this.error = errorMessage;
+            this.showPermissionIssue = true;
+          }
+        ),
+        switchMap(userId => {
+          return this.apiService.getCart(userId);
+        })
+      )
+      .subscribe((cartProducts: CartProduct[]) => {
+        console.log('cartProducts:', cartProducts);
+        this.cartService.setCart(cartProducts);
+      });
 
     this.loginForm.reset();
   }
