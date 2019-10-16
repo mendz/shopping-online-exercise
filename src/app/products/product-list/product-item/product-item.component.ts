00aa49b41d8cb21a3daf88c82d4e80fa16ba406a
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-10-16 23:03:57 +0300

    when adding product to cart also save the user id

--- src/app/products/product-list/product-item/product-item.component.ts
+++ src/app/products/product-list/product-item/product-item.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import { Product } from '../../product.model';
 import { CartService } from 'src/app/cart/cart.service';
 import { CartProduct } from 'src/app/cart/cart-product.model';
+import { ApiService } from 'src/app/api.service';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-product-item',
@@ -11,12 +14,17 @@ import { CartProduct } from 'src/app/cart/cart-product.model';
 export class ProductItemComponent implements OnInit {
   @Input() product: Product;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {}
 
   addToCart() {
+    const userId = this.authService.user.value.id;
     this.cartService.addToCart(
+      userId,
       new CartProduct(
         this.product.id,
         this.product.name,
