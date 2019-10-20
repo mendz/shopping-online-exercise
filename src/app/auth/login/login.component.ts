8fbc17f4e251805d637c63ecc477f32ae3b6f2f8
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2019-10-20 07:06:18 +0300

    add alert modal

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -90,4 +90,8 @@ export class LoginComponent implements OnInit {
       return 'red';
     }
   }
+
+  onHandleError() {
+    this.error = null;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,17 @@ import { ErrorBoxComponent } from './error-box/error-box.component';
 import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from './material.module';
 import { CenteredDirective } from './centered.directive';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
-  declarations: [ErrorBoxComponent, CenteredDirective],
+  declarations: [ErrorBoxComponent, CenteredDirective, AlertComponent],
   imports: [CommonModule, AppMaterialModule],
   exports: [
     CommonModule,
     AppMaterialModule,
     ErrorBoxComponent,
     CenteredDirective,
+    AlertComponent,
   ],
 })
 export class SharedModule {}
